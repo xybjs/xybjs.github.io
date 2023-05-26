9135f5ee8cf69e4236fb619be3abae5c50c2e86c
xybjs <xy18341711085@163.com>
2023-05-26 23:10:45 +0800

    Site updated: 2023-05-26 23:10:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/26/hello-world/","2023/05/26/test/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/26/test/","2023/05/26/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
